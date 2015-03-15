7741bd4abd49a96fc64bb7452a61de79c9f75b5f
Sarah McManus <mcmanus.sarah.a@gmail.com>
2015-03-15 11:25:34 -0400

    Fixed everything the linter caught

--- ex26.rb
+++ ex26.rb
@@ -1,24 +1,24 @@
 module Ex2
 
   # This function will break up words for us.
-  def Ex25.brak_words(stuff
+  def Ex25.brake_words(stuff)
     words = stuff.split(' ')
-    return word
+    return words
   end
 
   # Sorts the words.
-  def Ex25.sortwords(words)
+  def Ex25.sort_words(words)
     return words.sort
   end
 
   # Prints the first word after popping it off.
-  df Ex25.print_first_word(words)
+  def Ex25.print_first_word(words)
     word = words.pop(1)
-    puts wor
+    puts word
   end
 
   # Prints the last word after popping it off.
-  def Ex25:print_last_word(words)
+  def Ex25.print_last_word(words)
     word = words.pop
     put word
   end
@@ -27,11 +27,11 @@ module Ex2
   def Ex25.sort_sentence(sentence)
     words = Ex25.break_words(sentence)
     return Ex25.sort_words(words)
-  ed
+  end
 
   # Prints the first and last words of the sentence.
-  def Ex25.print_first_and_last(sentence
-    words = Ex25.break_words(sentenc)
+  def Ex25.print_first_and_last(sentence)
+    words = Ex25.break_words(sentence)
     Ex25.print_first_wrd(word)
     Ex25.print_last_word(words)
   end
@@ -55,39 +55,39 @@ cannot discern \n the needs of love
 nor comprehend passion from intuition
 and requires an explanation
 \n\t\twhere there is none.
-ENDED
+END
 
 puts "--------------"
 puts poem
 puts "--------------"
 
 
-five = 10 - 2  3 - 6
-puts "This should be five: #{five"
+five = 10 - 2 + 3 - 6
+puts "This should be five: #{five}"
 
 def secret_formula(started)
-  jelly_bens = started * 500
+  jelly_beans = started * 500
   jars = jelly_beans / 1000
-  crate = jars / 100
+  crates = jars / 100
   return jelly_beans, jars, crates
 end
 
 
 start_point = 10000
-beans, jars crates = secret_formula(start_point)
+beans, jars, crates = secret_formula(start_point)
 
 puts "With a starting point of: #{start_point}"
-puts "We'd have #{beans beans, #{jars} jars, and #{crates} crates."
+puts "We'd have #{beans} beans, #{jars} jars, and #{crates} crates."
 
 start_point = start_point / 10
 
 sentence = "All good things come to those who wait."
 words = Ex25.break_words(sentence)
 sorted_words = Ex25.sort_words(words)
-Ex25.print_first_word(wrds)
-Ex25.print_last_word words)
+Ex25.print_first_word(words)
+Ex25.print_last_word(words)
 Ex25.print_first_word(sort_words)
 Ex25.print_last_word(sorted_words)
-sorted_words = Ex25.sort_sentenc(sentence)
+sorted_words = Ex25.sort_sentence(sentence)
 Ex25.print_first_and_last(sentence)
-Ex25:print_first_and_last_sorted(sentence)
+Ex25.print_first_and_last_sorted(sentence)
